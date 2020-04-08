95494bd1b057491db122192667f590cc8817970e
Jack Burns <chugga151@gmail.com>
2020-04-08 18:53:39 +0100

    added service page

---
+++ precache-manifest.f9f775deceddf82470101260aa580e15.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a0b9583227134f1181341d5c26b1ffb9",
+    "url": "/jsm-showcase/index.html"
+  },
+  {
+    "revision": "019286d68a010a1a448a",
+    "url": "/jsm-showcase/static/css/2.4707e12a.chunk.css"
+  },
+  {
+    "revision": "3740ad605ac70ec377c5",
+    "url": "/jsm-showcase/static/css/main.ccf880c5.chunk.css"
+  },
+  {
+    "revision": "019286d68a010a1a448a",
+    "url": "/jsm-showcase/static/js/2.630a0107.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/jsm-showcase/static/js/2.630a0107.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3740ad605ac70ec377c5",
+    "url": "/jsm-showcase/static/js/main.6f3ed2c0.chunk.js"
+  },
+  {
+    "revision": "542f57ad1d07758259c0",
+    "url": "/jsm-showcase/static/js/runtime-main.a904c1b8.js"
+  },
+  {
+    "revision": "6a37b2404ddda9f83242fcec50a19593",
+    "url": "/jsm-showcase/static/media/cladding.6a37b240.jpg"
+  },
+  {
+    "revision": "8c426d0dc993c32ae171b1d47a82fc2f",
+    "url": "/jsm-showcase/static/media/conservatory.8c426d0d.jpg"
+  },
+  {
+    "revision": "4042ac9ba45703e28370a8ecb71fb997",
+    "url": "/jsm-showcase/static/media/driveway.4042ac9b.jpg"
+  },
+  {
+    "revision": "8aa7244c32d321dbd84890e3d223a80f",
+    "url": "/jsm-showcase/static/media/gilroy.8aa7244c.woff2"
+  },
+  {
+    "revision": "97a71bd2fedce901f7b36f47dd2b059f",
+    "url": "/jsm-showcase/static/media/gilroylight.97a71bd2.woff2"
+  },
+  {
+    "revision": "3bd12f690501caf04f93919052f60fba",
+    "url": "/jsm-showcase/static/media/guttering.3bd12f69.jpg"
+  },
+  {
+    "revision": "1335b8fc8066e4fff88a70794bac8fed",
+    "url": "/jsm-showcase/static/media/rain4.1335b8fc.jpg"
+  },
+  {
+    "revision": "5ba2a33861aa30256b031054b6fc2dcb",
+    "url": "/jsm-showcase/static/media/solar.5ba2a338.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/jsm-showcase/precache-manifest.9231a2d6db3277d1e3a805d3ba926800.js"
+  "/jsm-showcase/precache-manifest.f9f775deceddf82470101260aa580e15.js"
 );
 
 self.addEventListener('message', (event) => {
