142a6854912cd11e8d7b5d01af94899747ba542a
Jack Burns <chugga151@gmail.com>
2020-04-08 09:20:42 +0100

    initial prototype

---
+++ precache-manifest.24b4515183806e4c5fb81ce9fb4e48f9.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4e148812aebf4fa9a1a9a1fce11d72c1",
+    "url": "/jsm-prototype/index.html"
+  },
+  {
+    "revision": "99024ed6329cc92ab807",
+    "url": "/jsm-prototype/static/css/main.3aba485b.chunk.css"
+  },
+  {
+    "revision": "de42be9c5a0e9b27d5d5",
+    "url": "/jsm-prototype/static/js/2.e3f14a30.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/jsm-prototype/static/js/2.e3f14a30.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "99024ed6329cc92ab807",
+    "url": "/jsm-prototype/static/js/main.e7501cc3.chunk.js"
+  },
+  {
+    "revision": "f37a8ee51b5ad20526f2",
+    "url": "/jsm-prototype/static/js/runtime-main.0cd343fd.js"
+  },
+  {
+    "revision": "8aa7244c32d321dbd84890e3d223a80f",
+    "url": "/jsm-prototype/static/media/gilroy.8aa7244c.woff2"
+  },
+  {
+    "revision": "97a71bd2fedce901f7b36f47dd2b059f",
+    "url": "/jsm-prototype/static/media/gilroylight.97a71bd2.woff2"
+  },
+  {
+    "revision": "a45f35dc4e06726b7ccccfb2522ec2cb",
+    "url": "/jsm-prototype/static/media/rain.a45f35dc.jpg"
+  }
+]);
